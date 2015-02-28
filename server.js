394eca903f5a7e6bad5de19deda0748da2e65174
csquires <chandlersquires@yahoo.com>
2015-02-28 18:35:25 -0500

    venmo_challenge

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
 	 res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("HI world/n");
+  res.end(req.get("venmo_challenge"));
 })
 app.post('/', function(req,res) {
 

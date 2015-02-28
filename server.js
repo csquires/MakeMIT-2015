f96a8ee2985f7675111836ea6b8a532c245e0516
csquires <chandlersquires@yahoo.com>
2015-02-28 17:37:18 -0500

    HelloAgain

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var app = express()
 var port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.send(req.get("venmo_challenge"));
-  res.end("venmo_challenge");
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+  	//res.send(req.get("venmo_challenge"));
+	//res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.send('Hello World');
+	res.end();
 })
 app.post('/', function(req,res) {
 

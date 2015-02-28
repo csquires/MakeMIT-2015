cd3f3ec1172feb6a44409d434c32a7af46c6e159
csquires <chandlersquires@yahoo.com>
2015-02-28 18:08:51 -0500

    plzzzzwork

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ app.get('/', function (req, res) {
   res.end('Hello World!');
 })
 
-/*var server = app.listen(3000, function () {
+app.post('/', function (req,res){})
+
+var server = app.listen(3000, function () {
 
   var host = server.address().address
   var port = server.address().port
 
   console.log('Example app listening at http://%s:%s', host, port)
 
-})*/
+})

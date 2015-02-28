57081bb0dfefa91f2b6a08625f72d1c0b9812e58
csquires <chandlersquires@yahoo.com>
2015-02-28 16:52:32 -0500

    express app

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-
+/*
 var http = require('http')
 var port = process.env.PORT || 1337;
 
@@ -8,19 +8,19 @@ http.createServer(function(req, res) {
   res.end("HI world/n");
 }).listen(port);
 
+*/
 
 
 
 
-/*
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 1337;
+
 app.get('/', function (req, res) {
-//res.writeHead(200, { 'Content-Type': 'text/plain' });
-//res.write('venmo_challenge');
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-res.end('Hello World\n')
+	 res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end("HI world/n");
 })
 app.post('/', function(req,res) {
 
@@ -28,11 +28,5 @@ app.post('/', function(req,res) {
 
 var server = app.listen(port, function () {
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
 
 })
-
-*/

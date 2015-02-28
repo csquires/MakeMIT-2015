e597f0eb087d68a759a7f10b444b736637a01828
csquires <chandlersquires@yahoo.com>
2015-02-28 18:28:07 -0500

    work.

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 1337;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	 res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end("HI world/n");
+})
+app.post('/', function(req,res) {
+
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
 
 })

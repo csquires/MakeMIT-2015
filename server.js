a4862b1075ddc8f0d72c21fe11c2d3ce4051535e
Justin Carrus <carrus.justin@gmail.com>
2015-02-28 18:46:16 -0500

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.send(req.query.id);
+    res.send("Hello world");
 })
 app.post('/', function(req,res) {
 

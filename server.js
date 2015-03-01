8d84f81a27ee9a22b0e47dae0cd0914879fd0477
Justin Carrus <carrus.justin@gmail.com>
2015-02-28 19:21:48 -0500

    commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express()
 
 var port = process.env.PORT || 1337;
 
+var log = "";
+
 app.get('/webhook_url', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(req.query.venmo_challenge);
@@ -10,6 +12,15 @@ app.get('/webhook_url', function (req, res) {
 app.post('/', function(req,res) {
 
 })
+
+app.get('/test', function(req, res) {
+    log += req.query.val + "\n";
+    res.end("Hello world");
+}
+
+app.get('/log', function(req, res) {
+    res.end(log);
+};
 
 var server = app.listen(port, function () {
 

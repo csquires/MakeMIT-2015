2154b3d1cf75f601e806464f3240b60cbcf43d5e
Justin Carrus <carrus.justin@gmail.com>
2015-02-28 20:51:24 -0500

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var log = "";
 app.get('/webhook_url', function (req, res) {
     try{
 	log += "Hello World";
-	log += req.query + "\n";
+	log += req.query.data.note + "\n";
     } catch (e){}
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(req.query.venmo_challenge);

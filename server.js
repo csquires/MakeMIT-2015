8e5e602d1d8ec410a5b567c804b1eea69c6e508b
Justin Carrus <carrus.justin@gmail.com>
2015-03-01 00:23:23 -0500

    commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+1;5Bvar express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -48,7 +48,7 @@ app.post('/webhook_url', function (req, res) {
 /*	newstring = newstring.substring(
 	    newstring.search("chair ") + 6,
 	    newstring.search("chair ") + 7);
-*/	sb.send("Table 3rd Orders", "string", newstring); 
+*/	sb.send("Table 3rd Orders", "string", "3"); 
     } catch (e){}
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(req.query.venmo_challenge);
